e8d558975d4dc70e9501bd1647d85d451c57ce40
Rafał Huk <r.huk@outlook.com>
2019-10-24 12:07:20 +0200

    Pobranie przykładowych danych z API

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'ClinicClientApp';
+
+  values : any;
+  constructor(private http: HttpClient) { }
+  
+  ngOnInit(): void {
+    this.getValues();
+  }
+  getValues()
+  {
+    this.http.get('https://localhost:5001/doctor').subscribe(response => {
+      this.values = response;
+      console.log(this.values);
+
+    }, error => {
+      console.log(error);
+    })
+  }
 }
