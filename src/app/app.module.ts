bf998573686a369ffaf52d3767390744dd09d711
Rafał Huk <r.huk@outlook.com>
2019-10-29 19:34:52 +0100

    Nowa koncepcja

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
-import { HeaderComponent } from './header/header.component';
-import { NavComponent } from './header/nav/nav.component';
-import { BodyComponent } from './body/body.component';
-import { FooterComponent } from './footer/footer.component';
-import { LoginComponent } from './header/login/login.component';
+import { NavComponent } from './nav/nav.component';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    NavComponent,
-    BodyComponent,
-    LoginComponent,
-    FooterComponent
+    NavComponent
   ],
   imports: [
     BrowserModule,
