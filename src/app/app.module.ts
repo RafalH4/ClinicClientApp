7a9fa9cf12b30149fafa0dbb7d2cac49426f22a5
Rafał Huk <r.huk@outlook.com>
2019-10-29 16:29:53 +0100

    Dodanie frameworka AwesomeIcons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,18 @@ import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { HeaderComponent } from './header/header.component';
 import { NavComponent } from './header/nav/nav.component';
-import { LogoComponent } from './header/logo/logo.component';
+import { BodyComponent } from './body/body.component';
+import { FooterComponent } from './footer/footer.component';
+import { LoginComponent } from './header/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     NavComponent,
-    LogoComponent
+    BodyComponent,
+    LoginComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
