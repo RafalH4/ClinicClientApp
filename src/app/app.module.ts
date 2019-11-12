b4c6a51f5f21499cf3b90be7a33ae87391b376dd
Rafał Huk <r.huk@outlook.com>
2019-11-12 23:05:47 +0100

    Model formularza rejestracji pacjenta

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
@@ -22,7 +22,8 @@ import { RegisterComponent } from './register/register.component';
   imports: [
     BrowserModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [
     AuthService
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -7,13 +8,55 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
   model: any = {};
-
-  constructor() { }
-
-  ngOnInit() {
+  
+  modelForm : FormGroup;formErrors ={
+    firstName:'',
+    lastName:''
   }
 
+  private validationMessages = {
+    firstName : {
+      required : "firstName is required"
+    },
+    lastName : {
+      required : "lastName is required",
+      maxLength: "lastName must have at least 6 characters"
+    }
+  }
+
+  constructor(private formBuilder : FormBuilder) { }
+
+  ngOnInit() {
+    this.modelForm = this.formBuilder.group({
+      firstName: ['', Validators.required],
+      secondName: ['', Validators.required],
+      pesel: ['', Validators.required],
+
+      postCode: ['', Validators.required],
+      city: ['', Validators.required],
+      street: ['', Validators.required],
+      houseNumber: ['', Validators.required],
+      phoneNumber: ['', Validators.required],
+
+      email: ['', Validators.email],
+      password: ['', Validators.required],
+      password2: ['', Validators.required],
+
+    });
+  }
+
+  onControlValueChanged() {
+    const form = this.modelForm;
+
+    for(let field in this.formErrors) {
+      this.formErrors[field] ='';
+      let  control = form.get(field);
+
+      
+    }
+  }
   register() {
+    console.log("Hello");
     console.log(this.model);
   }
   cancel() {
