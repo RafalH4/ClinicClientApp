2a9e1441a2a037ce33d5f02304c7afac2b57cd14
Rafał Huk <r.huk@outlook.com>
2019-12-12 02:01:12 +0100

    Formularz generatora wizyt

--- src/app/content/appointments/appointments.component.ts
+++ src/app/content/appointments/appointments.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { DoctorsService } from 'src/app/services/userServices/doctors.service';
+import { DepartmentsService } from 'src/app/services/departments.service';
+import { MedOfficeService } from 'src/app/services/med-office.service';
 
 
 
@@ -10,9 +13,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppointmentsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private httpDoctor: DoctorsService,
+    private httpMedOffice: MedOfficeService) { }
+  doctors: any[];
+  medOffices: any[];
+  dates = new Array<Date>();
+  startDate: Date;
+  endDate : Date;
 
   ngOnInit() {
+    this.httpMedOffice.getMedOffices().subscribe((data: any[]) => {
+      this.medOffices = data;
+      console.log(this.medOffices)
+    })
+    this.httpDoctor.getAllDoctors().subscribe((data: any[]) => {
+      this.doctors = data;
+    })
   }
 
+  newRange(){
+    
+    var now = new Date();
+    var daysOfYear = [];
+    for (var d = new Date(this.startDate); d <=new Date(this.endDate); d.setDate(d.getDate() + 1)) {
+        this.dates.push(new Date(d));
+        //console.log(d);
+    }
+
+    console.log(this.dates);
+
+  }
+  deleteDate(i){
+    console.log(i);
+    this.dates.splice(i, i+1);
+    console.log( this.dates);
+  }
+ 
+
 }
+
